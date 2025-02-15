e6f844302f59b76d604c2dcd12a88c177ee2de26
cbnsndwch <8313760+cbnsndwch@users.noreply.github.com>
2025-02-15 16:35:44 -0500

    feat(source-mongo-server): add Swagger UI to standalone Mongo Source app

--- apps/source-mongodb-server/src/main.ts
+++ apps/source-mongodb-server/src/main.ts
@@ -1,23 +1,38 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { WsAdapter } from '@nestjs/platform-ws';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 import { corsDelegate } from '@cbnsndwch/zero-contracts';
 
 import { AppModule } from './app.module.js';
+import { setupSwaggerUi } from './utils/oas.js';
+import { makeTable } from './utils/string-utils.js';
 
 const logger = new Logger('Bootstrap');
 
+const PORT = process.env.PORT ?? 8000;
+
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule, {
+    const app = await NestFactory.create<NestExpressApplication>(AppModule, {
         cors: corsDelegate
     });
 
     app.useWebSocketAdapter(new WsAdapter(app));
 
-    await app.listen(process.env.PORT ?? 8000);
+    setupSwaggerUi(app);
 
-    logger.log(`Application is running on: ${await app.getUrl()}`);
+    await app.listen(PORT);
+
+    const baseUrl = await app.getUrl();
+    const banner = makeTable(
+        {
+            'Base URL': baseUrl,
+            SwaggerUI: `${baseUrl}/docs`
+        },
+        `🚀 Server running 🚀`
+    );
+    banner.forEach(line => logger.log(line));
 }
 
 void bootstrap();
---
+++ apps/source-mongodb-server/src/utils/oas.ts
@@ -0,0 +1,28 @@
+import path from 'node:path';
+
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+export function setupSwaggerUi(app: NestExpressApplication) {
+    // serve static assets (if any)
+    app.useStaticAssets(path.resolve('public'));
+
+    const options = new DocumentBuilder()
+        .setTitle('Zero Source Mongo')
+        .setDescription('A MongoDB Custom Source for Zero Cache')
+        .setVersion('0.1.0')
+        .addBasicAuth()
+        .addBearerAuth()
+        //    .addSecurityRequirements('bearer')
+        .build();
+
+    const document = SwaggerModule.createDocument(app, options);
+
+    SwaggerModule.setup('docs', app, document, {
+        swaggerOptions: {
+            persistAuthorization: true
+        },
+        customSiteTitle: 'Zero Source Mongo API Docs',
+        customCssUrl: '/css/oas-theme.css'
+    });
+}
