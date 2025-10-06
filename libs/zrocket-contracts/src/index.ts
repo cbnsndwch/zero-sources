b220eb6f5e5986f73fc0484a90ba09493567eb29
cbnsndwch <8313760+cbnsndwch@users.noreply.github.com>
2025-10-06 19:19:21 -0400

    feat(contracts): export queries from main index
    Update main index to export all query definitions including:
    - Query context types and utilities
    - Private room queries (myChats, myGroups, chatById, groupById)
    
    Changes export from './queries/context.js' to './queries/index.js'
    to include all query modules.
    
    Related to #71

--- libs/zrocket-contracts/src/index.ts
+++ libs/zrocket-contracts/src/index.ts
@@ -3,6 +3,6 @@ export * from './auth/index.js';
 export * from './common/index.js';
 
 export * from './messages/index.js';
-export * from './queries/context.js';
+export * from './queries/index.js';
 export * from './rooms/index.js';
 export * from './users/index.js';
